f0e214883044c02f883ae53c2bf36ca7ce3c48aa
rh222fh <SEY9Gh54n2mss2V>
2018-03-09 01:32:14 +0100

    Testing palette

--- src/main/MovieInfo.js
+++ src/main/MovieInfo.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Header, Image, Modal } from 'semantic-ui-react';
+import Palette from 'react-palette';
 
 const MovieInfo = props => {
 	const imgUrl = `https://image.tmdb.org/t/p/w342/${props.movie.poster_path}`;
 
 	return (
-		<Modal
-			closeOnDimmerClick={true}
-			closeOnEscape={true}
-			open={props.bool}
-			onClose={props.onClose}
-		>
-			{/*<Modal.Header>{props.movie.title}</Modal.Header>*/}
-			<Modal.Content image>
-				<Image style={{ maxWidth: 250, maxHeight: 350 }} src={imgUrl} />
-				<Modal.Description>
-					<Header>{props.movie.title}</Header>
-					<p style={{ textAlign: 'justify' }}>
-						{props.movie.overview}
-					</p>
-				</Modal.Description>
-			</Modal.Content>
-		</Modal>
+		<Palette image={imgUrl}>
+			{palette => (
+				<Modal
+					closeOnDimmerClick={true}
+					closeOnEscape={true}
+					open={props.bool}
+					onClose={props.onClose}
+					size='{tiny}'
+					style={{ backgroundColor: palette.muted }}
+				>
+					{/*<Modal.Header>{props.movie.title}</Modal.Header>*/}
+					<Modal.Content
+						image
+						style={{ backgroundColor: palette.muted }}
+					>
+						<Image
+							style={{ maxWidth: 250, maxHeight: 350 }}
+							src={imgUrl}
+						/>
+						<Modal.Description>
+							<Header>{props.movie.title}</Header>
+							<p style={{ textAlign: 'justify' }}>
+								{props.movie.overview}
+							</p>
+						</Modal.Description>
+					</Modal.Content>
+				</Modal>
+			)}
+		</Palette>
 	);
 };
 
--- src/main/MovieListItem.js
+++ src/main/MovieListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import './MovieListItem.css';
+import Palette from 'react-palette';
 
 const MovieListItem = ({ movie, clicked }) => {
 	const { title, poster_path, release_date, vote_average } = movie;
@@ -8,16 +9,29 @@ const MovieListItem = ({ movie, clicked }) => {
 	const year = release_date.substring(0, 4);
 
 	return (
-		<Card style={{ margin: 10, maxWidth: 200 }} onClick={clicked}>
-			<Image style={{ maxWidth: 200 }} src={imgUrl} />
-			<Card.Content>
-				<Card.Header style={{ fontSize: '12pt' }}>{title}</Card.Header>
-			</Card.Content>
-			<Card.Content extra style={{ fontWeight: 'bold' }}>
-				<span className='date'>{year}</span>
-				<span style={{ float: 'right' }}>{vote_average}</span>
-			</Card.Content>
-		</Card>
+		<Palette image={imgUrl}>
+			{palette => (
+				<Card
+					style={{
+						backgroundColor : palette.muted,
+						margin          : 10,
+						maxWidth        : 200
+					}}
+					onClick={clicked}
+				>
+					<Image style={{ maxWidth: 200 }} src={imgUrl} />
+					<Card.Content>
+						<Card.Header style={{ fontSize: '12pt' }}>
+							{title}
+						</Card.Header>
+					</Card.Content>
+					<Card.Content extra style={{ fontWeight: 'bold' }}>
+						<span className='date'>{year}</span>
+						<span style={{ float: 'right' }}>{vote_average}</span>
+					</Card.Content>
+				</Card>
+			)}
+		</Palette>
 	);
 };
 export default MovieListItem;
